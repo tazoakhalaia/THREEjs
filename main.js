88ec4519105ceba520ba41e174949f2b74d80bf7
tazoakhalaia <darknemiroff@gmail.com>
2024-03-16 12:45:00 +0400

    install dat.gui

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import * as THREE from "three";
+import * as dat from 'dat.gui'
 import { Mesh } from "three";
 import { GridHelper } from "three";
+import { MeshStandardMaterial } from "three";
 import { SphereGeometry } from "three";
 import { PlaneGeometry } from "three";
 import { BoxGeometry } from "three";
 import { MeshBasicMaterial } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
 
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -45,10 +48,15 @@ const plane = new Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
 scene.add(plane);
 
-const sphereGeometry = new SphereGeometry(4);
-const sphereMaterial = new MeshBasicMaterial({ color: "blue" });
+const sphereGeometry = new SphereGeometry(4,50,50);
+const sphereMaterial = new MeshBasicMaterial({
+  color: "blue",
+  wireframe: false,
+});
 const sphere = new Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+
+sphere.position.set(-10,10,0)
 
 function animate() {
   box.rotation.x += 0.01;
